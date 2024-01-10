ecba66282e480c71e75ace092e9a1e25484441fd
Mohammed Ahmed Babiker <m.babiker@k-devs.com>
2024-01-10 15:35:49 +0300

    Update project structure and add a console application as aexample

---
+++ example/SuperInject.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SuperInject.Console;
+using SuperInject.Extensions;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, service) =>
+    {
+        service.AddSuperInject();
+    }).Build();
+
+
+var myService = host.Services.GetRequiredService<IMyService>();
+
+// Print result to console
+myService.DoSomething();
+Console.ReadLine();
